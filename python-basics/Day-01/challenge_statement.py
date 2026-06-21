d49299ff19ec00e5bdef0a06f9975d7975c8d28c
Shivangi Gupta <shivi.ganga.19@gmail.com>
2026-06-22 00:39:33 +0530

    Day 1: Setup + first python program

---
+++ python-basics/Day-01/challenge_statement.py
@@ -0,0 +1,11 @@
+# Write a statement that prints "Hello World!"
+
+print("Hello World!")
+
+# Write a statement that prints "Have a good day."
+
+print("Have a good day.")
+
+# Write a statement that prints "Learning Python is fun!"
+
+print("Learning Python is fun!")
